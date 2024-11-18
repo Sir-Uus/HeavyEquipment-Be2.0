4a19b7ef6f991205b3327a607962b930097929cf
unknown <firdauswho7@gmail.com>
2024-11-18 09:31:33 +0700

    update rules validation

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7369211c7c883ca6debbdf81c08dc593088fc11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdf309352ebd3a6b8970ff190f49cdb7c6d83de2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
